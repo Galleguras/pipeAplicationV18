dfba9e8f74607a75d2c3a976e420454fb31d88b0
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-12-29 14:23:57 +0100

    141 PrimeNg MenuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+//prime-ng
+/* import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card'; */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PrimeNgModule /* ButtonModule, CardModule */],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
