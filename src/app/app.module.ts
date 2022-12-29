b06e4f8f79d28138fe2a747f805f8fd4f4560218
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-12-29 16:55:32 +0100

    142 -rutas de nuestra aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
+import { AppRouterModule } from './app-router.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
+import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
+import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 //prime-ng
 /* import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card'; */
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule /* ButtonModule, CardModule */],
+  declarations: [
+    AppComponent,
+    NumerosComponent,
+    NoComunesComponent,
+    BasicosComponent,
+    OrdenarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRouterModule /* ButtonModule, CardModule */,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
