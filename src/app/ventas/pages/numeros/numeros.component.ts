3f510e3d49fbb76e69c3e863ce37a5bd17bd551d
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-12-30 14:46:19 +0100

    pipe number

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styleUrls: ['./numeros.component.css']
+  styleUrls: ['./numeros.component.css'],
 })
 export class NumerosComponent {
-
+  ventasNetas: number = 6345123123.3398;
+  porcentaje: number = 0.48;
 }
