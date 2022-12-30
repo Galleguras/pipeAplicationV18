40a51b6cfa6fd776b419f8734bf033e95bb259e2
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-12-30 14:23:05 +0100

    locales de la apliccion para pipe de fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { VentasModule } from './ventas/ventas.module';
 import { AppRouterModule } from './app-router.module';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
+//CAMBIAR EL LOCALE DE LA APP
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
