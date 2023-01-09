cfcd7cb8a6f4b2ccdd9f53e2955034171ba7863f
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2023-01-09 13:10:55 +0100

    153,154 pipes i18nSelect i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styleUrls: ['./no-comunes.component.css']
+  styleUrls: ['./no-comunes.component.css'],
 })
 export class NoComunesComponent {
+  //i18nSelect
+  nombre: string = 'Susana';
+  genero: string = 'femenino';
+  invitacionMapa = { masculino: 'invitarlo', femenino: 'invitarla' };
 
+  //i18nPlural
+  clientes = ['qwe', 'asd', 'asdas', 'asfafs'];
+  clientesMapa = {
+    '=0': 'no tenemos ninguno',
+    '=1': ' un cliente esperando',
+    '=2': ' 2 clientes esperando',
+    other: ' # clientes esperando',
+  };
 }
